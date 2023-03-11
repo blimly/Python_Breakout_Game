4bceb3a741fd30c8aa962085fd0c05c9b90aad3c
Gorane7 <kristjankv32@gmail.com>
2023-03-11 16:24:50 +0200

    Added ball that bounces from paddle

--- breakout.py
+++ breakout.py
@@ -18,16 +18,53 @@ class Paddle:
         self.location += 10 * self.moving
 
 
+class Ball:
+    def __init__(self, x, y, game, vx, vy):
+        self.x = x
+        self.y = y
+        self.game = game
+        self.vx = vx
+        self.vy = vy
+        self.radius = 20
+        self.colour = (0, 255, 0)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, self.colour, (self.x, self.y), self.radius)
+
+    def update(self):
+        self.move()
+        self.handle_collision()
+
+    def move(self):
+        self.x += self.vx
+        self.y += self.vy
+
+    def handle_collision(self):
+        if self.x < self.radius or self.x > self.game.width - self.radius:
+            self.vertical_collision()
+        at_paddle = self.game.paddle.location - self.game.paddle.width / 2 < self.x < self.game.paddle.location + self.game.paddle.width / 2
+        if self.y < self.radius or self.y > self.game.height - self.radius - self.game.paddle.height and at_paddle:
+            self.horizontal_collision()
+
+    def vertical_collision(self):
+        self.vx = - self.vx
+
+    def horizontal_collision(self):
+        self.vy = - self.vy
+
 class Game:
     def __init__(self):
         # Test
         pygame.init()
+        self.width = 640
+        self.height = 480
         self.screen = pygame.display.set_mode((640, 480))
         pygame.display.set_caption("Breakout Game")
         self.running = True
         self.tick_length = 1 / 30
         self.BACKGROUND_COLOUR = (20, 20, 50)
         self.paddle = Paddle((640 - 60) // 2, 20, 60, (150, 150, 255))
+        self.ball = Ball(self.width // 2, self.height // 2, self, random.random() * 4 - 2, 10)
 
     def input(self):
         for event in pygame.event.get():
@@ -46,10 +83,12 @@ class Game:
 
     def update(self):
         self.paddle.move()
+        self.ball.update()
 
     def render(self):
         self.screen.fill(self.BACKGROUND_COLOUR)
         self.paddle.draw(self.screen)
+        self.ball.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
