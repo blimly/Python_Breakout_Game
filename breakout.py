81430a38b86b6b931284159d0a4e00443e882ede
Gorane7 <kristjankv32@gmail.com>
2023-03-11 16:11:24 +0200

    Added paddle that moves

--- breakout.py
+++ breakout.py
@@ -3,6 +3,21 @@ import random
 import time
 
 
+class Paddle:
+    def __init__(self, initial_location, height, width, colour):
+        self.location = initial_location
+        self.height = height
+        self.width = width
+        self.colour = colour
+        self.moving = 0
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.colour, (self.location - self.width // 2, 480 - self.height, self.width, self.height))
+
+    def move(self):
+        self.location += 10 * self.moving
+
+
 class Game:
     def __init__(self):
         # Test
@@ -12,6 +27,7 @@ class Game:
         self.running = True
         self.tick_length = 1 / 30
         self.BACKGROUND_COLOUR = (20, 20, 50)
+        self.paddle = Paddle((640 - 60) // 2, 20, 60, (150, 150, 255))
 
     def input(self):
         for event in pygame.event.get():
@@ -20,12 +36,20 @@ class Game:
             if event.type == pygame.KEYDOWN:
                 if event.key in [pygame.K_q, pygame.K_ESCAPE]:
                     self.running = False
+                if event.key == pygame.K_a:
+                    self.paddle.moving = -1
+                if event.key == pygame.K_d:
+                    self.paddle.moving = 1
+            if event.type == pygame.KEYUP:
+                if event.key in [pygame.K_a, pygame.K_d]:
+                    self.paddle.moving = 0
 
     def update(self):
-        pass
+        self.paddle.move()
 
     def render(self):
         self.screen.fill(self.BACKGROUND_COLOUR)
+        self.paddle.draw(self.screen)
         pygame.display.flip()
 
     def run(self):
@@ -34,7 +58,7 @@ class Game:
             self.input()
             self.update()
             self.render()
-            time.sleep(self.tick_length - (time.time() - start))
+            time.sleep(max(0, self.tick_length - (time.time() - start)))
         pygame.quit()
 
 
